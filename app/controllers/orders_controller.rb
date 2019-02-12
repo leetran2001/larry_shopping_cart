e35568d31cb04bdc35fd5e25a65cd6a409cc8b74
Juan Pablo Cuevas <jp.cuevaslavin@gmail.com>
2019-02-12 12:32:21 -0300

    added quantity

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,20 @@
 class OrdersController < ApplicationController
 
   def create
-    order = Order.new(
-      product_id: params[:product_id],
-      user: current_user
-    )
-    order.save
+    @product = Product.find(params[:product_id])
+    @order = Order.find_or_initialize_by(product: @product, user: current_user, paid: false)
+    @order.quantity += 1
+    @order.save
     redirect_to products_path, notice: 'Producto agregado al carro!'
+
+    # @order = Order.find_by(product: @product, user: current_user, paid: false)
+    # @order = Order.new(product: @product, user: current_user, paid: false) unless @order.present?
   end
 
   def cart
-    @orders = current_user.orders
+    # Refactor
+    @orders = current_user.orders.where(paid: false)
+    @total = @orders.inject(0) { |sum, order| sum + order.product.price * order.quantity }
   end
-  
   
 end
